b5156557d1ed547c223809e349b6fa43576bf6b2
Rocky <rockytan@users.noreply.github.com>
2025-01-10 13:48:30 +0800

    删除baseurl代码

--- prompts/common.ts
+++ prompts/common.ts
@@ -17,9 +17,6 @@ import { checkValid, checkWhitelist } from "./quota.ts";
 const apiKey = Deno.env.get("OPENAI_API_KEY");
 assert(apiKey, "failed to get openAI API key");
 
-//const baseURL = Deno.env.get("OPENAI_BASE_URL");
-//assert(baseURL, "failed to get openAI baseURL");
-
 const openai = new OpenAI({
   apiKey: apiKey,
   baseURL: 'https://oa.api2d.net'
